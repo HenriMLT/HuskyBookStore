25c4de556baaae94713814793baa0c0d7dc08d6b
Felipe Resk <fe.resk@hotmail.com>
2024-05-23 16:06:28 -0300

    [FEAT] - Termino de set para api e algumas estilizações

--- JS/script.js
+++ JS/script.js
@@ -7,7 +7,7 @@ function getBooks1(){
     .then(response => response.json())
     .then(response => {
         console.log(response)
-        const livrosListDiv = document.querySelector("#livros");
+        const livrosListDiv = document.querySelector("#livros-1");
         
         
         response.forEach(livro => livrosListDiv.insertAdjacentHTML("beforeend", `
@@ -32,7 +32,7 @@ function getBooks2(){
     .then(response => response.json())
     .then(response => {
         console.log(response)
-        const livrosListDiv = document.querySelector("#livros-1");
+        const livrosListDiv = document.querySelector("#livros-2");
         
         
         response.forEach(livro => livrosListDiv.insertAdjacentHTML("beforeend", `
@@ -57,7 +57,57 @@ function getBooks3(){
     .then(response => response.json())
     .then(response => {
         console.log(response)
-        const livrosListDiv = document.querySelector("#livros-2");
+        const livrosListDiv = document.querySelector("#livros-3");
+        
+        
+        response.forEach(livro => livrosListDiv.insertAdjacentHTML("beforeend", `
+            <div class="livro">
+                <img src="${livro.linkImagem}">
+                <p class="nome-livro">${livro.titulo}</p>
+                <p class="avaliacao-livro">${'&#9733;&#9733;&#9733;&#9733;&#9734;'}</p>
+                <p class="preco-livro">${livro.preco}</p>
+                <button class="botao-comprar-livro">Comprar &#8594;</button>
+            </div>
+        `));
+    })
+    
+}
+
+function getBooks4(){
+    fetch("http://localhost:8080/livros", {
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        },
+    })
+    .then(response => response.json())
+    .then(response => {
+        console.log(response)
+        const livrosListDiv = document.querySelector("#livros-4");
+        
+        
+        response.forEach(livro => livrosListDiv.insertAdjacentHTML("beforeend", `
+            <div class="livro">
+                <img src="${livro.linkImagem}">
+                <p class="nome-livro">${livro.titulo}</p>
+                <p class="avaliacao-livro">${'&#9733;&#9733;&#9733;&#9733;&#9734;'}</p>
+                <p class="preco-livro">${livro.preco}</p>
+                <button class="botao-comprar-livro">Comprar &#8594;</button>
+            </div>
+        `));
+    })
+    
+}
+
+function getBooks5(){
+    fetch("http://localhost:8080/livros", {
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        },
+    })
+    .then(response => response.json())
+    .then(response => {
+        console.log(response)
+        const livrosListDiv = document.querySelector("#livros-5");
         
         
         response.forEach(livro => livrosListDiv.insertAdjacentHTML("beforeend", `
@@ -77,5 +127,8 @@ window.addEventListener("load", evento => {
     console.log('load concluido')
     getBooks1();
     getBooks2();
+    getBooks3();
+    getBooks4();
+    getBooks5();
     
     });
